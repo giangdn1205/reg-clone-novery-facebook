26ca65e11c4ce7a731093f1d51677cbd3794cd26
= <=>
2022-12-09 21:13:02 +0700

    upload file

---
+++ module/tesst.py
@@ -0,0 +1,10 @@
+import unidecode # pip install unidecode
+
+def convert_name(name):
+	lower = name.lower()
+	name = lower.strip().split(' ')
+	return unidecode.unidecode(''.join(name))
+
+print(convert_name("Đặng Đình Đoàn")) #dangdinhdoan
+
+
